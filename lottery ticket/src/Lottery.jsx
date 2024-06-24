97d0bbaf07757f64c6ea4d0bb5cf221a5339d9a9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-06-24 22:32:03 +0530

    Files

---
+++ lottery ticket/src/Lottery.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./Lottery.css"
+import { genTicket, sum } from "./helper";
+
+function Lottery (){
+    let [ticket, setTicket] = useState(genTicket(3));
+    let isWinning = sum(ticket) === 15;
+
+    let buyTicket = () =>{
+        setTicket(genTicket(3));
+    }
+
+    return (
+        <div>
+            <h2>Lottery Game</h2>
+            <div className="ticket">
+              <span>{ticket[0]}</span>
+              <span>{ticket[1]}</span>
+              <span>{ticket[2]}</span>
+            </div>
+            <br></br>
+            <button onClick={buyTicket}>Buy New Ticket</button>
+            <h2>{isWinning && "Congratulations, you won!"}</h2>
+        </div>
+    );
+}
+
+export default Lottery;
---
+++ lottery ticket/src/helper.js
@@ -0,0 +1,15 @@
+//generate ticket 
+function genTicket (n){ //user defined number
+    let arr = new Array(n); //n numbers
+    for (let i =0; i<n; i++){
+        arr[i] = Math.floor(Math.random() * 10); //creating random number of n size that user gives
+    }
+    return arr;
+}
+
+//sum of ticket 
+function sum(arr){
+    return arr.reduce((sum, curr) => sum + curr, 0);
+}
+
+export {genTicket, sum};
